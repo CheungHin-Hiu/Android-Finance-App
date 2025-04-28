dce18753f060baa28291a1dbe0594dbf3427b0a2
hoyinMok <hymokae@connect.ust.hk>
2025-04-28 13:52:02 +0800

    class: updated template for api route definition

--- Backend/API/api_router.py
+++ Backend/API/api_router.py
@@ -7,9 +7,27 @@ from ..API.model.GET_finance_data import RequestFinanceData
 
 
 class APIRouteDefintion:
-    def __init__(self):
+    def __init__(self, router: fastapi.APIRouter):
+        self.router = router
+        
+        # route defintion
+        self.router.add_api_route("/login", self._get_login_operation, methods=["GET"])
+
+
+
+    # endpoint: _____/finance, method: GET
+    async def _get_login_operation(self):
         pass
 
     # endpoint: _____/finance, method: GET
-    async def GET_finance_data(self):
-    
+    async def _get_register_operation(self):
+        pass
+
+
+    # endpoint: _____/finance, method: GET
+    async def _get_finance_data(self):
+        pass
+
+
+
+
