1e4be28d04255211a2455e68ed1cb017f4e63a26
Alex-ChanCM <cmchanbb@connect.ust.hk>
2025-05-03 22:40:27 +0800

    fix error

--- Frontend/app/src/main/java/com/example/androidfinanceapp/ui/Overview/IncomeAndExpenseScreen.kt
+++ Frontend/app/src/main/java/com/example/androidfinanceapp/ui/Overview/IncomeAndExpenseScreen.kt
@@ -1,9 +1,6 @@
 package com.example.androidfinanceapp.ui.Overview
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
